edaee30b35b5188308b52e0347984481aeba8970
Kenneth Fullbright (フルブライト　ケネス) <74458502+kennyneedsmilky@users.noreply.github.com>
2021-08-09 13:25:52 +0900

    Touch is now working but buggy

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,11 +11,15 @@ let drawWidth = "2";
 let isDrawing = false;
 
 /** Event Handler */
+document.addEventListener('touchstart',function(e){ mapTouchEvents(e,'mousedown'); },true);
+document.addEventListener('touchmove',function(e){ mapTouchEvents(e,'mousemove'); },true);
+document.addEventListener('touchend',function(e){ mapTouchEvents(e,'mouseup'); },true);
+document.addEventListener('touchcancel',function(e){ mapTouchEvents(e,'mouseup'); },true);
 
 // Events for both mouse and touch.
-canvas01.addEventListener("touchstart", startDrawing, false);
-canvas01.addEventListener("touchmove", drawing, false);
-canvas01.addEventListener("touchend", stopDrawing, false);
+// canvas01.addEventListener("touchstart", startDrawing, false);
+// canvas01.addEventListener("touchmove", drawing, false);
+// canvas01.addEventListener("touchend", stopDrawing, false);
 
 canvas01.addEventListener("mousedown", startDrawing, false);
 canvas01.addEventListener("mousemove", drawing, false);
@@ -27,7 +31,7 @@ canvas01.addEventListener("mouseout", stopDrawing, false);
 // Clear Canvas
 function clearCanvas() {
     canvas01_ctx.fillStyle = "white"; // Canvas 01 Context Fillstyle
-    canvas01_ctx.fillRect(0, 0, canvas01.width, canvas01.height); 
+    canvas01_ctx.fillRect(0, 0, canvas01.width, canvas01.height);
 }
 
 // Start Drawing
@@ -61,4 +65,40 @@ function stopDrawing(e) {
         canvas01_ctx.closePath();
         isDrawing = false;
     }
+}
+
+// Map Touch Events
+function mapTouchEvents(event,simulatedType) {
+	
+	//Ignore any mapping if more than 1 fingers touching 
+	if(event.changedTouches.length>1){return;}
+	
+	var touch = event.changedTouches[0];
+	
+	//--https://developer.mozilla.org/en/DOM/document.createEvent--
+	let eventToSimulate = document.createEvent('MouseEvent');
+	
+	//--https://developer.mozilla.org/en-US/docs/Web/API/event.initMouseEvent--
+	eventToSimulate.initMouseEvent(
+		simulatedType,		//type
+		true,				//bubbles
+		true,				//cancelable
+		window,				//view
+		1,					//detail
+		touch.screenX,		//screenX
+		touch.screenY,		//screenY
+		touch.clientX,		//clientX
+		touch.clientY,		//clientY
+		false,				//ctrlKey
+		false,				//altKey
+		false,				//shiftKey
+		false,				//metaKey
+		0,					//button
+		null				//relatedTarget
+	);
+
+	touch.target.dispatchEvent(eventToSimulate);
+	//This ignores the default scroll behavior
+	event.preventDefault();				
+	
 }
